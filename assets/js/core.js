d45548e61224400d937ba0f17ea0639a0517d567
Zyplos <officialzyplos@gmail.com>
2017-04-30 20:18:20 -0500

    Window Loader/Navbar Fixed

---
+++ assets/js/core.js
@@ -0,0 +1,13 @@
+console.log('%c --------------------------------------', 'color: #ff3e3e;');
+console.log('%c rude', 'color: #ff3e3e;');
+console.log('%c ', 'color: #ff3e3e;');
+console.log('%c snooping around the console like that', 'color: #ff3e3e;');
+console.log('%c Just kidding.', 'color: #ff3e3e;');
+console.log('%c That\'s okay, if anything, we encourage it! Check out our Github at: https://github.com/2501CodeLab/', 'color: #ff3e3e;');
+console.log('%c All of our projects\' code is usually there.', 'color: #ff3e3e;');
+console.log('%c Found a bug? Broke something? Got an error? Report it to us!', 'color: #ff3e3e;');
+console.log('%c --------------------------------------', 'color: #ff3e3e;');
+//
+$(window).on('load', function(e) {
+    $("#loader_wrapper").fadeOut();
+});
